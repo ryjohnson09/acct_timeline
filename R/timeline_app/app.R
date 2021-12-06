2ce984e3e7218ba72661a72ba2949da4f99f5655
ryjohnson09 <johnsrc06@gmail.com>
2021-12-06 20:03:38 +0000

    incluce all accounts

--- R/timeline_app/app.R
+++ R/timeline_app/app.R
@@ -10,7 +10,7 @@ library(scales)
 board <- board_rsconnect(server = Sys.getenv("CONNECT_SERVER"))
 timeline_data <- pin_read(board, "ryan/acct_timeline_data")
 
-# Month/Year Data
+# Format Month/Year Data -----------------------
 date_range <- seq(lubridate::today() - months(12),
                         lubridate::today() + months(2), by='month')
 
@@ -19,14 +19,16 @@ month_year_data <- tibble(floor_date(date_range, unit = "month")) %>%
   mutate(year_label = year(date_range))
 
 
+# UI -------------------------------------------
 ui <- fillPage(column(12, align="center",
                       # Pod Filter
-                      
+                      selectInput("pod", "",
+                                  choices = sort(unique(timeline_data$acct_pod)),
+                                  selected = "Ent LSH"),
                       
                       # Account Name
-                      selectInput("acct_name", "", 
-                                  choices = sort(unique(timeline_data$acct_name)), 
-                                  selected = "Amgen"),
+                      uiOutput("acct_name"),
+                      
                       # Engagement Type
                       checkboxGroupInput("engagement_type", "",
                                          choices = c("Sales", "CS", "Support"), 
@@ -39,25 +41,41 @@ ui <- fillPage(column(12, align="center",
                                  )
 )
 
+# Server -------------------------------------------
 server <- function(input, output, session) {
   
-  # filter data
-  timeline_data_filt <- reactive({
+  # Filter by pod
+  pod_filt <- reactive({
     timeline_data %>% 
-      # Filter for account name
-      filter(acct_name == input$acct_name) %>% 
+      # Filter for pod
+      filter(acct_pod == input$pod)
+  })
+  
+  # Generate reactive list of accounts
+  output$acct_name <- renderUI({
+    selectInput("acct_name_list", "", 
+                choices = pod_filt()$acct_name, 
+                selected = pod_filt()$acct_name[1])
+  })
+  
+  # Filter data by accounts
+  acct_filt <- reactive({
+    timeline_data %>% 
+      filter(acct_name %in% input$acct_name_list) %>% 
       # Filter for date range
       filter(between(event_date, min(date_range), max(date_range))) %>% 
       # Filter for point type
       filter(event_type %in% input$engagement_type)
   })
   
+  # Plot output
   output$plot <- renderPlot({
     
     # Time plot
-    ggplot(data = timeline_data_filt(), aes(y = event_date, 
-                               x = x_axis, 
-                               label = event_label)) +
+    ggplot(data = acct_filt(), 
+           aes(y = event_date,
+               x = x_axis,
+               label = event_label)) +
       theme_minimal() +
       geom_vline(xintercept=0, 
                  color = "black", 
@@ -74,13 +92,13 @@ server <- function(input, output, session) {
             legend.position = "none"
       ) +
       # Sales points
-      geom_point(data = filter(timeline_data_filt(), event_type == "Sales"),
+      geom_point(data = filter(acct_filt(), event_type == "Sales"),
                        fill = "#f9c74f", pch = 21, size = 3, color = "black") +
       # CS points
-      geom_point(data = filter(timeline_data_filt(), event_type == "CS"),
+      geom_point(data = filter(acct_filt(), event_type == "CS"),
                  fill = "#4d908e", pch = 21, size = 3, color = "black") +
       # Support points
-      geom_point(data = filter(timeline_data_filt(), event_type == "Support"),
+      geom_point(data = filter(acct_filt(), event_type == "Support"),
                  fill = "#f94144", pch = 21, size = 3, color = "black") +
       # Date labels
       geom_text(data = month_year_data, 
@@ -95,11 +113,11 @@ server <- function(input, output, session) {
                 size = 3, hjust = "right",
                 color = "grey") +
       # Point labels
-      geom_label_repel(data = filter(timeline_data_filt(), event_type == "Sales"),
+      geom_label_repel(data = filter(acct_filt(), event_type == "Sales"),
                        aes(fill = event_label), nudge_x = -0.7, size = 2) +
-      geom_label_repel(data = filter(timeline_data_filt(), event_type == "CS"),
+      geom_label_repel(data = filter(acct_filt(), event_type == "CS"),
                        aes(fill = event_subtype),nudge_x = 0.6, size = 2) +
-      geom_label_repel(data = filter(timeline_data_filt(), event_type == "Support"),
+      geom_label_repel(data = filter(acct_filt(), event_type == "Support"),
                        aes(fill = event_subtype),nudge_x = -0.3, size = 2)
   }, res = 96)
   
@@ -112,15 +130,16 @@ server <- function(input, output, session) {
   })
   
   
-  # Table under plot
+  # Popup Information (gt table)
   output$data <- render_gt({
-    nearPoints(timeline_data_filt(), input$plot_click) %>% 
+    nearPoints(acct_filt(), input$plot_click) %>% 
       gt() %>%
       cols_hide(
         columns = c(
           event_type, x_axis, event_acct_id, 
           acct_name, event_date, event_label,
-          event_subtype
+          event_subtype, acct_owner_id, acct_cs_owner_id, acct_pod,
+          acct_owner_name, acct_cs_owner_name
         )
       ) %>% 
       fmt_markdown(columns = event_notes) %>% 
